b257782f3581490a0cadd4f205fb903cd740544e
Ashok S <ashok.s@koenig-solutions.com>
2025-12-10 15:36:28 +0530

    spring boot with cassandra

---
+++ Module-20/cassandra-demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> allProducts(){
+        return productService.getAllProducts();
+    }
+}
---
+++ Module-20/cassandra-demo/src/main/java/com/example/demo/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Product;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends CassandraRepository<Product, UUID> {
+}
