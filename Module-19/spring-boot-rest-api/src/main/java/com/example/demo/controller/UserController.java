d11851bafcf81716757127564b14e6c4fea7da78
Ashok S <ashok.s@koenig-solutions.com>
2025-12-10 14:01:57 +0530

    spring boot rest api

--- Module-19/spring-boot-rest-api/src/main/java/com/example/demo/controller/UserController.java
+++ Module-19/spring-boot-rest-api/src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,33 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping
+    public String getAllUsers(){
+        return "List of users";
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody String newUser){
+        return "User created with request body: "+ newUser;
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable String id){
+        return "User detail with id: " + id;
+    }
+
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody String editedUser){
+        return "User updated with request body: " + editedUser + " with id : "+ id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUserById(@PathVariable String id){
+        return "User deleted with id: "+ id;
+    }
 }
