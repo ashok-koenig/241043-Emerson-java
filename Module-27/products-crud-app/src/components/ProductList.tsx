9ad657f6407122119e4f1c0e35090e8c6ec344f6
Ashok S <ashok.s@koenig-solutions.com>
2025-12-17 11:35:29 +0530

    create api call

--- Module-27/products-crud-app/src/components/ProductList.tsx
+++ Module-27/products-crud-app/src/components/ProductList.tsx
@@ -8,16 +8,20 @@ export default function ProductList(props: Props) {
   return (
     <div>
         <table style={{border: "2px solid black"}}>
-            <tr>
-                <th>Product name</th>
-                <th>Product price</th>
-            </tr>
+            <thead>
+                <tr>
+                    <th>Product name</th>
+                    <th>Product price</th>
+                </tr>
+            </thead>
+            <tbody>
             {
-                props.products.map((product) => (<tr>
+                props.products.map((product) => (<tr key={product.id}>
                     <td>{product.name}</td>
                     <td>{product.price}</td>
                 </tr>))
             }
+            </tbody>
         </table>
     </div>
   )
--- Module-27/products-crud-app/src/services/productApi.ts
+++ Module-27/products-crud-app/src/services/productApi.ts
@@ -1,5 +1,14 @@
+import type Product from "../models/Product";
 
 export const getProducts = async () => {
     const response = await fetch("http://localhost:8080/products")
     return response.json();
+}
+
+export const createProduct= async (newProduct: Product) => {
+    await fetch("http://localhost:8080/products", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newProduct)
+    })
 }
