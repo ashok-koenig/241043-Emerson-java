fa0284cdd31846988cf1f528f40baadf5dcb1d26
Ashok S <ashok.s@koenig-solutions.com>
2026-01-20 10:41:47 +0530

    priority demo

---
+++ Module-09/Multithreading-Demo/src/ThreadPriorityDemo.java
@@ -0,0 +1,24 @@
+class PriorityThread extends Thread {
+    public PriorityThread(String name){
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() +" Priority: "+ getPriority());
+    }
+}
+
+public class ThreadPriorityDemo {
+    public static void main(String[] args) {
+        PriorityThread t1 = new PriorityThread("Low");
+        PriorityThread t2 = new PriorityThread("High");
+
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+
+        t1.start();
+        t2.start();
+    }
+
+}
