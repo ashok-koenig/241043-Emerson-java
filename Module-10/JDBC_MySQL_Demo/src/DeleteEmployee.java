7107e0788dffa121d89a5d68a8c475b1c2dfcce3
Ashok S <ashok.s@koenig-solutions.com>
2026-01-21 11:35:43 +0530

    delete employees

---
+++ Module-10/JDBC_MySQL_Demo/src/DeleteEmployee.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteEmployee {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/company";
+        String username = "root";
+        String password = "root";
+        try{
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement stmt = conn.createStatement();
+            String query = "DELETE FROM employees WHERE id=3";
+            int deletedRows = stmt.executeUpdate(query);
+            System.out.println("Rows Deleted: "+ deletedRows);
+            stmt.close();
+            conn.close();
+
+        }catch (SQLException e){
+            System.out.println("Error: "+ e);
+        }
+    }
+}
